877bf5f435e16c71990d5cdb89b6eeff37490dbf
RanR112 <146650544+RanR112@users.noreply.github.com>
2024-07-11 00:03:28 +0700

    .

--- src/Contents/Home/Home.jsx
+++ src/Contents/Home/Home.jsx
@@ -13,16 +13,12 @@ const Home = () => {
                 <div className="textContent grid">
                     <h1 data-aos="fade-up">Ayo, Mulai Petualangan Belajarmu Bersama  <span>BIMBA  IFE  !</span></h1>
                     <h4 data-aos="fade-up">Gabung dengan pengajar keren dan teman baru untuk petualangan belajar yang menyenangkan dan penuh prestasi!</h4>
-                    <div className='btn' data-aos="fade-up">
-                    <h1>Ayo, Mulai Petualangan Belajarmu Bersama  <span>BIMBA  IFE  !</span></h1>
-                    <h4>Gabung dengan pengajar keren dan teman baru untuk petualangan belajar yang menyenangkan dan penuh prestasi!</h4>
                     <div className='btn'>
                         <a href="#PENDAFTARAN" className='btnDaftar'>
                             <p>Daftar</p>
                         </a>
                     </div>
                 </div>
-            </div>
             </div>
             <div className="bot">
                 <img src={bot} alt="botimg" className='botimg' />
