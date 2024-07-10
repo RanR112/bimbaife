378fae79a5fc60e4706f32a2cedcf536cc457a45
RanR112 <146650544+RanR112@users.noreply.github.com>
2024-07-11 00:01:33 +0700

    .

--- src/Contents/Home/Home.jsx
+++ src/Contents/Home/Home.jsx
@@ -11,20 +11,18 @@ const Home = () => {
             <div className="mid flex">
                 <img src={utama} alt="logo" className='logo' data-aos="fade-up"/>
                 <div className="textContent grid">
-<<<<<<< HEAD
                     <h1 data-aos="fade-up">Ayo, Mulai Petualangan Belajarmu Bersama  <span>BIMBA  IFE  !</span></h1>
                     <h4 data-aos="fade-up">Gabung dengan pengajar keren dan teman baru untuk petualangan belajar yang menyenangkan dan penuh prestasi!</h4>
                     <div className='btn' data-aos="fade-up">
-=======
                     <h1>Ayo, Mulai Petualangan Belajarmu Bersama  <span>BIMBA  IFE  !</span></h1>
                     <h4>Gabung dengan pengajar keren dan teman baru untuk petualangan belajar yang menyenangkan dan penuh prestasi!</h4>
                     <div className='btn'>
->>>>>>> 08765e5e8f4786442410f6aef08c1e76d57474e6
                         <a href="#PENDAFTARAN" className='btnDaftar'>
                             <p>Daftar</p>
                         </a>
                     </div>
                 </div>
+            </div>
             </div>
             <div className="bot">
                 <img src={bot} alt="botimg" className='botimg' />
