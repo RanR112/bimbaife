b99d240bc5b0e5965f5f64f1480a52c21304c8a9
Randy Rafael <146650544+RanR112@users.noreply.github.com>
2024-07-10 08:40:30 +0700

    Add files via upload

---
+++ src/Contents/Program/Program.jsx
@@ -0,0 +1,117 @@
+import React, {useEffect, useState} from 'react'
+import "./program.css"
+import { doodle } from "../../Assets"
+import { AiFillSchedule } from "react-icons/ai";
+import { FaCalendarAlt } from "react-icons/fa";
+import { IoTime } from "react-icons/io5";
+import { FaMoneyBills } from "react-icons/fa6";
+import { FaMoneyBillWave } from "react-icons/fa";
+import image from "../../Assets/program.png"
+import image2 from "../../Assets/program2.png"
+
+const Program = () => {
+
+    const [currentImage, setCurrentImage] = useState(image);
+
+    useEffect(() => {
+        const updateImage = () => {
+            if (window.innerWidth <= 885) {
+                setCurrentImage(image2);
+            } else {
+                setCurrentImage(image);
+            }
+        };
+
+        updateImage(); // Run on initial load
+        window.addEventListener('resize', updateImage); // Add event listener for window resize
+
+        return () => window.removeEventListener('resize', updateImage); // Clean up on component unmount
+    }, []);
+
+    return (
+        <section className="program" id='PROGRAM'>
+            <img src={doodle} alt="" />
+            <div className="content">
+                <img src={currentImage} alt="program" className='image'/>
+            </div>
+        </section>
+    );
+    // return (
+    //     <section className='program' id='PROGRAM'>
+    //         <img src={doodle} alt="" />
+    //         {/* <div className="layer grid">
+    //             <h1 className='title'>PROGRAM <span>STUDI</span></h1>
+    //             <hr className='line'/>
+    //             <div className="content">
+    //                 <div className="kelas">
+    //                     <h3><AiFillSchedule className='iconTitle'/> JADWAL</h3>
+    //                     <div className="container flex">
+    //                         <div className="textContent">
+    //                             <h4>PROGRAM CALISTUNG</h4>
+    //                             <h5><FaCalendarAlt className='icon'/> Hari Senin - Jumat</h5>
+    //                             <ul>
+    //                                 <li><IoTime className='icon'/> Kelas Lion : 07.30 - 09.00</li>
+    //                                 <li><IoTime className='icon'/> Kelas Rabbit : 09.00 - 10.30</li>
+    //                                 <li><IoTime className='icon'/> Kelas Dino : 10.30 - 12.00</li>
+    //                             </ul>
+    //                         </div>
+    //                         <hr className='line2'/>
+    //                         <div className="textContent">
+    //                             <h4>PROGRAM ENGLISH</h4>
+    //                             <h5><FaCalendarAlt className='icon'/> Hari Rabu - Sabtu</h5>
+    //                             <ul>
+    //                                 <li><IoTime className='icon'/> Kelas Siang : 14.30 - 15.30</li>
+    //                                 <li><IoTime className='icon'/> Kelas  Sore: 16.00 - 17.00</li>
+    //                                 <li><IoTime className='icon'/> Kelas Sabtu : 07.30 sd 11.30</li>
+    //                             </ul>
+    //                         </div>
+    //                         <hr className='line2'/>
+    //                         <div className="textContent">
+    //                             <h4>PROGRAM JARIMATIKA</h4>
+    //                             <h5><FaCalendarAlt className='icon'/> Hari Selasa - Sabtu</h5>
+    //                             <ul>
+    //                                 <li><IoTime className='icon'/> Kelas Siang : 14.30 - 15.30</li>
+    //                                 <li><IoTime className='icon'/> Kelas  Sore: 16.00 - 17.00</li>
+    //                                 <li><IoTime className='icon'/> Kelas Sabtu : 07.30 sd 11.30</li>
+    //                             </ul>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //                 <div className="spp">
+    //                     <h3><FaMoneyBills className='iconTitle'/> SPP</h3>
+    //                     <div className="container flex">
+    //                         <div className="textContent">
+    //                             <h4>PROGRAM CALISTUNG</h4>
+    //                             <ul>
+    //                                 <li><FaMoneyBillWave className='icon'/> 3x Pertemuan : Rp 220.000/Bulan</li>
+    //                                 <li><FaMoneyBillWave className='icon'/> 4x Pertemuan : Rp 230.000/Bulan</li>
+    //                                 <li><FaMoneyBillWave className='icon'/> 5x Pertemuan : Rp 240.000/Bulan</li>
+    //                             </ul>
+    //                         </div>
+    //                         <hr className='line2'/>
+    //                         <div className="textContent">
+    //                             <h4>PROGRAM ENGLISH</h4>
+    //                             <ul>
+    //                                 <li><FaMoneyBillWave className='icon'/> 2x Pertemuan : Rp 160.000/Bulan</li>
+    //                                 <li><FaMoneyBillWave className='icon'/> 3x Pertemuan : Rp 170.000/Bulan</li>
+    //                                 <li className='none'>.</li>
+    //                             </ul>
+    //                         </div>
+    //                         <hr className='line2'/>
+    //                         <div className="textContent">
+    //                             <h4>PROGRAM JARIMATIKA</h4>
+    //                             <ul>
+    //                                 <li><FaMoneyBillWave className='icon'/> 2x Pertemuan : Rp 160.000/Bulan</li>
+    //                                 <li><FaMoneyBillWave className='icon'/> 3x Pertemuan : Rp 170.000/Bulan</li>
+    //                                 <li className='none'>.</li>
+    //                             </ul>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div> */}
+    //     </section>
+    // )
+}
+
+export default Program
---
+++ src/Contents/Tentang/Tentang.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./tentang.css"
+import { tentang1, tentang2, tentang3 } from '../../Assets'
+
+const Tentang = () => {
+    return (
+        <section className="tentang" id='TENTANG'>
+            <h1 className="title">TENTANG KAMI</h1>
+            <div className="apa flex">
+                <img src={tentang1} alt="" />
+                <div className="textContent grid">
+                    <h3>Apa itu<span> BIMBA <span className='IFE'>IFE</span> ?</span></h3>
+                    <p>Bimba IFE adalah bimbingan belajar terkemuka yang berkomitmen untuk memberikan pendidikan berkualitas tinggi bagi anak-anak. Di Bimba IFE, kami percaya bahwa setiap anak memiliki potensi luar biasa yang perlu dikembangkan sejak dini. Oleh karena itu, kami menawarkan tiga program unggulan yang dirancang untuk memenuhi kebutuhan pendidikan anak Anda: Baca Tulis Hitung (Calistung), Bahasa Inggris, dan Jarimatika.</p>
+                    <h4>Fasilitas Kami</h4>
+                    <ul>
+                        <li>Ruangan berAC</li>
+                        <li>Ruangan bersih dan nyaman</li>
+                        <li>Berbagai macam mainan</li>
+                        <li>Berbagai gambar untuk mewarnai</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="kenapa flex">
+                <img src={tentang2} alt="" />
+                <div className="textContent grid">
+                    <h3>Mengapa memilih<span> BIMBA <span className='IFE'>IFE</span> ?</span></h3>
+                    <ul>
+                        <li>Jadwal belajar yg seru, selain Calistung setiap hri anak akan mendapatkan kegiatan yg berbeda beda seperti  kreatifitas/montessori/b.arab/b.english,dsb</li>
+                        <li>Pengajar Berpengalaman: Tim pengajar di Bimba IFE terdiri dari para profesional yang berpengalaman dan berdedikasi untuk membantu anak-anak mencapai potensi maksimal mereka.</li>
+                        <li>Metode Pembelajaran Interaktif: Kami menggunakan metode pembelajaran yang interaktif dan menyenangkan untuk memastikan anak-anak belajar dengan cara yang efektif dan tidak membosankan.</li>
+                        <li>Lingkungan Belajar yang Nyaman: Bimba IFE menyediakan lingkungan belajar yang aman dan nyaman, yang memungkinkan anak-anak belajar dengan tenang dan fokus.</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="visimisi flex">
+                <img src={tentang3} alt="" />
+                <div className="textContent grid">
+                    <h3>Visi & Misi<span> BIMBA <span className='IFE'>IFE</span> ?</span></h3>
+                    <h4>Visi</h4>
+                    <p>Menjadi bimbingan belajar terdepan yang berkomitmen untuk mengembangkan potensi akademik dan kreativitas anak-anak Indonesia melalui metode pembelajaran yang inovatif dan menyenangkan.</p>
+                    <h4>Misi</h4>
+                    <ul>
+                        <li>Memberikan Pendidikan Berkualitas Tinggi</li>
+                        <li>Mengembangkan Potensi Setiap Anak</li>
+                        <li>Menghadirkan Pengajar Profesional dan Berpengalaman</li>
+                        <li>Mengintegrasikan Teknologi dalam Pembelajaran</li>
+                        <li>Mempromosikan Pendidikan Karakter</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Tentang
